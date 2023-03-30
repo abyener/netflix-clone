0b8f82d527b922d957dcce230bd9950dc16d3481
ABY <abyener54@gmail.com>
2023-03-30 13:18:32 +0300

    Add nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import "../Nav.css";
+import NetflixLogo from "../assets/images/NetflixLogo.png";
+import ProfileImage from "../assets/images/ProfileImage.png";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else handleShow(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"} `}>
+      <div className="nav_contents">
+        <img src={NetflixLogo} alt="" className="nav_logo" />
+        <img src={ProfileImage} alt="" className="nav_avatar" />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
