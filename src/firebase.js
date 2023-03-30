29cc0f97af3765d0681121d20b222eeb824a67de
ABY <abyener54@gmail.com>
2023-03-30 12:26:49 +0300

    Add project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUcj5Xcel_8_VgukiKHDf7zvp05bMUPAM",
+  authDomain: "netflix-clone-346e2.firebaseapp.com",
+  projectId: "netflix-clone-346e2",
+  storageBucket: "netflix-clone-346e2.appspot.com",
+  messagingSenderId: "254378275082",
+  appId: "1:254378275082:web:8a531b8d1e4963f164caf3",
+};
